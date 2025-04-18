af66ee42e6d8e88f99ae7e2e094dd2dd329f1724
lamba <lam45883@gmail.com>
2025-04-19 04:19:39 +0700

    change login url

--- vite.config.js
+++ vite.config.js
@@ -8,12 +8,5 @@ export default defineConfig({
     host: "0.0.0.0",
     port: 5173,
     strictPort: true,
-    proxy: {
-      "/api/v1": {
-        target: "https://crm-backend-bz03.onrender.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/v1/, "/api/v1"),
-      },
-    },
   },
 });
