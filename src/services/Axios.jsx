6cf94e4d83c4e246c751a793510556643b0df8d7
lamba <lam45883@gmail.com>
2025-04-24 13:01:13 +0700

    Fix RBAC

--- src/services/Axios.jsx
+++ src/services/Axios.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "https://auth.leadmastercrm.pro/api/v1",
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "https://auth.leadmastercrm.pro/api/v1",
+  // baseURL: "http://localhost:3000/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
